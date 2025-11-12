d58a392d34bf2b64361156c552cddb8866ffc79d
Clarity <claridadjulyfranz@gmail.com>
2025-11-13 03:30:04 +0800

    Delete src directory

--- src/app/about/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'About Me - My Portfolio',
-  description: 'Learn more about my background, skills, and why you should work with me.',
-};
-
-export default function AboutLayout({ children }) {
-  return children;
-}
